bca7830d0c83a80716f365d748b7371deb7c672f
ikkromm18 <muhammadikkrom749@gmail.com>
2023-10-30 20:01:16 +0700

    Menambahkan project ke-dalam git

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { merge } from 'webpack-merge';
+import common from './webpack.common';
+
+export default merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
